3f86baa589bb9c813c8b2fc08981d13c2cd77531
Hamza Outmassint <hamzaayrom@gmail.com>
2025-04-16 15:57:32 +0100

    feat: Refactor main function to include input handling with scanner

--- main.go
+++ main.go
@@ -1,9 +1,32 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
 	fmt.Println("welcom to OUT-CLI")
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+
+		fmt.Print("out-cli > ")
+
+		// Exit if there's an error reading input
+		if !scanner.Scan() {
+			break
+		}
+
+		input := strings.TrimSpace(scanner.Text())
+
+		if input == "" {
+			continue
+		}
+
+		fmt.Println(input)
+	}
 }
