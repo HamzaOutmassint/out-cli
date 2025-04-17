abebf5f3ba4658b925d6f6e173d820478ee25160
Hamza Outmassint <hamzaayrom@gmail.com>
2025-04-17 01:10:05 +0100

    feat: Update README and enhance main function with command execution

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# out-cli
+# Out-CLI
 
 A custom command-line application built with Go, designed to streamline the management of directories and files on Windows.
--- main.go
+++ main.go
@@ -8,12 +8,11 @@ import (
 )
 
 func main() {
-	fmt.Println("welcom to OUT-CLI")
+	fmt.Println("OUT-CLI, For more information type 'help'")
 
 	scanner := bufio.NewScanner(os.Stdin)
 
 	for {
-
 		fmt.Print("out-cli > ")
 
 		// Exit if there's an error reading input
@@ -22,11 +21,53 @@ func main() {
 		}
 
 		input := strings.TrimSpace(scanner.Text())
-
 		if input == "" {
 			continue
 		}
 
-		fmt.Println(input)
+		execution(input)
 	}
+}
+
+func execution(s string){
+	args := strings.Fields(s)
+	command := args[0]
+
+	if len(args) == 1 {
+		switch command {
+			case "help":
+				printHelp()
+			case "ls":
+				listFiles(".")
+			default: 
+				fmt.Printf("'%s' is not an out-cli command. See 'help'.\n",args[0])
+		}
+	}else if len(args) > 1 && len(args) < 3 {
+		fmt.Println(command)
+		fmt.Println(args[1])
+
+		switch command{
+			case "ls":
+				listFiles(args[1])
+		}
+	}else {
+		fmt.Println("many args")
+	}
+
+}
+
+func printHelp(){
+	fmt.Println()
+	fmt.Println("Available commands:")
+    fmt.Println("  help       		- Show this help message")
+    fmt.Println("  cr         		- Create a directory (usage: cr <directory>)")
+    fmt.Println("  cd         		- changes the current directory (usage: cd <directory>)")
+    fmt.Println("  ls         		- List files in a directory (usage: ls)")
+    fmt.Println("  rm         		- Delete a file or directory (usage: rm <path>)")
+    fmt.Println("  exit or Ctrl+C        - Exit the program")
+	fmt.Println()
+}
+
+func listFiles(s string){
+
 }
