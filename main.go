4c31eb00a8fa84294c603afa73a7b7f114ace63e
Hamza Outmassint <hamzaayrom@gmail.com>
2025-04-20 22:09:40 +0100

    feat: Refactor command execution and error handling; implement command registry

--- main.go
+++ main.go
@@ -1,3 +1,229 @@
+// package main
+
+// import (
+// 	"bufio"
+// 	"fmt"
+// 	"os"
+// 	"os/exec"
+// 	"strings"
+
+// 	"github.com/fatih/color"
+// )
+
+// var yellow = color.New(color.FgYellow).SprintFunc()
+// var green = color.New(color.FgGreen).SprintFunc()
+// var blue = color.New(color.FgBlue).SprintFunc()
+
+// func main() {
+// 	fmt.Println()
+// 	color.Green("  >_ OUT-CLI, For more information type `help`")
+// 	fmt.Println()
+
+// 	scanner := bufio.NewScanner(os.Stdin)
+
+// 	for {
+// 		currentDir, err := os.Getwd()
+// 		if err != nil {
+// 			color.Red("Error getting current directory:", err)
+// 			break
+// 		}
+
+// 		fmt.Print(currentDir, " >_ ")
+
+// 		// Exit if there's an error reading input
+// 		if !scanner.Scan() {
+// 			break
+// 		}
+
+// 		input := strings.TrimSpace(scanner.Text())
+// 		if input == "" {
+// 			continue
+// 		}
+
+// 		executeCommand(input)
+// 	}
+// }
+
+// // executeCommand parses and executes the user's input command.
+// func executeCommand(input string) {
+// 	args := strings.Fields(input)
+// 	command := args[0]
+
+// 	if len(args) == 1 {
+// 		switch command {
+// 		case "help":
+// 			displayHelpMenu()
+// 		case "ls":
+// 			listDirectoryContents(".")
+// 		case "/":
+// 			changeToHomeDirectory()
+// 		case "cls":
+// 			clearScreen()
+// 		case "exit":
+// 			os.Exit(0)
+// 		default:
+// 			fmt.Println()
+// 			fmt.Println(color.RedString("  >_  "), yellow(args[0]), color.RedString(" is not an out-cli command. See "), green("`help`"))
+// 			fmt.Println()
+// 		}
+// 	} else if len(args) > 1 && len(args) < 3 {
+// 		switch command {
+// 		case "ls":
+// 			listDirectoryContents(args[1])
+// 		case "cd":
+// 			changeCurrentDirectory(args[1])
+// 		case "rm":
+// 			removeFile(args[1:])
+// 		case "rmd":
+// 			removeDirectory(args[1:])
+// 		case "mkd":
+// 			createDirectory(args[1:])
+// 		default:
+// 			fmt.Println()
+// 			fmt.Println(color.RedString("  >_  "), yellow(args[0]), color.RedString(" is not an out-cli command. See "), green("`help`"))
+// 			fmt.Println()
+// 		}
+// 	} else if len(args) > 1 {
+// 		switch command {
+// 		case "mkd":
+// 			createDirectory(args[1:])
+// 		case "rm":
+// 			removeFile(args[1:])
+// 		case "rmd":
+// 			removeDirectory(args[1:])
+// 		default:
+// 			fmt.Println()
+// 			fmt.Println(color.RedString("  >_  "), yellow(args[0]), color.RedString(" is not an out-cli command. See "), green("`help`"))
+// 			fmt.Println()
+// 		}
+// 	}
+// }
+
+// // displayHelpMenu prints the help message for the CLI.
+// func displayHelpMenu() {
+// 	fmt.Println()
+// 	color.Blue("  Available commands:")
+
+// 	fmt.Printf("    %s       		 - Show this help message (usage: `%s`)\n", yellow("help"), yellow("help"))
+
+// 	fmt.Printf("    %s                  - Create one or more directories or files (usage: `%s`)\n", yellow("mkd"), yellow("mkd <path> [<path>...]"))
+
+// 	fmt.Printf("    %s         		 - Change the current directory (usage: `%s`)\n", yellow("cd"), yellow("cd <path>"))
+
+// 	fmt.Printf("    %s         		 - List files in a directory (usage: `%s`)\n", yellow("ls"), yellow("ls <path>"))
+
+// 	fmt.Printf("    %s         		 - Delete one or more files (usage: `%s`)\n", yellow("rm"), yellow("rm <path> [<path>...]"))
+
+// 	fmt.Printf("    %s                  - Delete one or more directories (usage: `%s`)\n", yellow("rmd"), yellow("rmd <path> [<path>...]"))
+
+// 	fmt.Printf("    %s          		 - Quickly navigate to the home directory (usage: `%s`)\n", yellow("/"), yellow("/"))
+
+// 	fmt.Printf("    %s          	 - Clear the terminal screen (usage: `%s`)\n", yellow("cls"), yellow("cls"))
+
+// 	fmt.Printf("    %s or %s       - Exit the program (usage: `%s` or `%s`)\n", yellow("exit"), yellow("Crtl+C"), yellow("exit"), yellow("Crtl+C"))
+
+// 	fmt.Println()
+// }
+
+// // createDirectory creates a new directory with the specified name.
+// func createDirectory(n []string) {
+// 	for _, v := range n {
+// 		err := os.Mkdir(v, 0744)
+
+// 		if err != nil {
+// 			fmt.Println()
+// 			fmt.Printf(color.RedString("  >_  Error - mkd "), "`%s`", color.RedString(": Cannot create a file when that file already exists.\n"), yellow(v))
+// 			fmt.Println()
+// 		}
+// 	}
+// }
+
+// // changeCurrentDirectory changes the current working directory to the specified path.
+// func changeCurrentDirectory(path string) {
+// 	err := os.Chdir(path)
+
+// 	if err != nil {
+// 		fmt.Println()
+// 		fmt.Println(color.RedString("  >_   Error - cd: Unable to access "), yellow(path), color.RedString(" - Directory not found.\n"))
+// 		fmt.Println()
+// 	}
+// }
+
+// // listDirectoryContents lists the files and directories in the specified path.
+// func listDirectoryContents(path string) {
+// 	entries, err := os.ReadDir(path)
+
+// 	fmt.Println()
+// 	if err != nil {
+// 		fmt.Println(color.RedString("  >_   Error - ls: Unable to access "), yellow(path), color.RedString(" - Directory not found.\n"))
+// 	} else if len(entries) == 0 {
+// 		color.Red("  >_  Empty directory")
+// 	} else {
+// 		for _, entry := range entries {
+// 			info, err := entry.Info()
+
+// 			if err != nil {
+// 				fmt.Println(err)
+// 			}
+
+// 			if entry.IsDir() {
+// 				fmt.Println("  >   ", info.ModTime().Format("02/01/2006"), green("               <dir>                  "), yellow(entry.Name()))
+// 			} else {
+// 				fmt.Println("  >   ", info.ModTime().Format("02/01/2006"), blue("               <file>                  "), yellow(entry.Name()))
+// 			}
+// 		}
+// 	}
+// 	fmt.Println()
+// }
+
+// // remove a file with a specified name.
+// func removeFile(n []string) {
+// 	for _, v := range n {
+// 		err := os.Remove(v)
+
+// 		if err != nil {
+// 			fmt.Println()
+// 			color.Red("  >_  Error: ", err)
+// 			fmt.Println()
+// 		}
+// 	}
+// }
+
+// // remove a directory with a specified name.
+// func removeDirectory(n []string) {
+// 	for _, v := range n {
+// 		err := os.RemoveAll(v)
+
+// 		if err != nil {
+// 			fmt.Println()
+// 			color.Red("  >_  Error: ", err)
+// 			fmt.Println()
+// 		}
+// 	}
+// }
+
+// // changeToHomeDirectory changes the current directory to the user's home directory.
+// func changeToHomeDirectory() {
+// 	homeDir, err := os.UserHomeDir()
+
+// 	if err != nil {
+// 		color.Red("  >_  Error: ", err)
+// 		return
+// 	}
+
+// 	err = os.Chdir(homeDir)
+
+// 	if err != nil {
+// 		color.Red("  >_  Error: ", err)
+// 	}
+// }
+
+// func clearScreen() {
+// 	cmd := exec.Command("cmd", "/c", "cls")
+// 	cmd.Stdout = os.Stdout
+// 	cmd.Run()
+// }
+
 package main
 
 import (
@@ -10,9 +236,28 @@ import (
 	"github.com/fatih/color"
 )
 
-var yellow = color.New(color.FgYellow).SprintFunc()
-var green = color.New(color.FgGreen).SprintFunc()
-var blue = color.New(color.FgBlue).SprintFunc()
+// CommandHandler defines the signature for command handler functions.
+type CommandHandler func(args []string) error
+
+// commandRegistry maps commands to their respective handler functions.
+var commandRegistry = map[string]CommandHandler{
+	"help": displayHelpMenu,
+	"ls":   listDirectoryContents,
+	"/":    changeToHomeDirectory,
+	"cls":  clearScreen,
+	"exit": exitCLI,
+	"cd":   changeCurrentDirectory,
+	"mkd":  createDirectory,
+	"mkf":  createFile,
+	"rmf":  removeFile,
+	"rmd":  removeDirectory,
+}
+
+// Utility Functions for Colored Output
+func red(s string) string    { return color.New(color.FgRed).SprintFunc()(s) }
+func yellow(s string) string { return color.New(color.FgYellow).SprintFunc()(s) }
+func green(s string) string  { return color.New(color.FgGreen).SprintFunc()(s) }
+func blue(s string) string   { return color.New(color.FgBlue).SprintFunc()(s) }
 
 func main() {
 	fmt.Println()
@@ -24,7 +269,7 @@ func main() {
 	for {
 		currentDir, err := os.Getwd()
 		if err != nil {
-			color.Red("Error getting current directory:", err)
+			printError(fmt.Sprintf("Error getting current directory: %v", err))
 			break
 		}
 
@@ -47,179 +292,155 @@ func main() {
 // executeCommand parses and executes the user's input command.
 func executeCommand(input string) {
 	args := strings.Fields(input)
-	command := args[0]
+	if len(args) == 0 {
+		return
+	}
 
-	if len(args) == 1 {
-		switch command {
-		case "help":
-			displayHelpMenu()
-		case "ls":
-			listDirectoryContents(".")
-		case "/":
-			changeToHomeDirectory()
-		case "cls":
-			clearScreen()
-		case "exit":
-			os.Exit(0)
-		default:
-			fmt.Println()
-			fmt.Println(color.RedString("  >_  "), yellow(args[0]), color.RedString(" is not an out-cli command. See "), green("`help`"))
-			fmt.Println()
-		}
-	} else if len(args) > 1 && len(args) < 3 {
-		switch command {
-		case "ls":
-			listDirectoryContents(args[1])
-		case "cd":
-			changeCurrentDirectory(args[1])
-		case "rm":
-			removeFile(args[1:])
-		case "rmd":
-			removeDirectory(args[1:])
-		case "mkd":
-			createDirectory(args[1:])
-		default:
-			fmt.Println()
-			fmt.Println(color.RedString("  >_  "), yellow(args[0]), color.RedString(" is not an out-cli command. See "), green("`help`"))
-			fmt.Println()
-		}
-	} else if len(args) > 1 {
-		switch command {
-		case "mkd":
-			createDirectory(args[1:])
-		case "rm":
-			removeFile(args[1:])
-		case "rmd":
-			removeDirectory(args[1:])
-		default:
-			fmt.Println()
-			fmt.Println(color.RedString("  >_  "), yellow(args[0]), color.RedString(" is not an out-cli command. See "), green("`help`"))
-			fmt.Println()
-		}
+	command := args[0]
+	handler, exists := commandRegistry[command]
+	if !exists {
+		printError(fmt.Sprintln(yellow(args[0]), color.RedString(" is not an out-cli command. See "), green("`help`")))
+		return
+	}
+
+	// Execute the command with the remaining arguments
+	err := handler(args[1:])
+	if err != nil {
+		printError(err.Error())
 	}
 }
 
+// printError prints an error message in red color.
+func printError(message string) {
+	fmt.Println()
+	fmt.Println(red("  >_  Error "), message)
+	fmt.Println()
+}
+
 // displayHelpMenu prints the help message for the CLI.
-func displayHelpMenu() {
+func displayHelpMenu(_ []string) error {
 	fmt.Println()
 	color.Blue("  Available commands:")
 
 	fmt.Printf("    %s       		 - Show this help message (usage: `%s`)\n", yellow("help"), yellow("help"))
-
 	fmt.Printf("    %s                  - Create one or more directories or files (usage: `%s`)\n", yellow("mkd"), yellow("mkd <path> [<path>...]"))
-
 	fmt.Printf("    %s         		 - Change the current directory (usage: `%s`)\n", yellow("cd"), yellow("cd <path>"))
-
 	fmt.Printf("    %s         		 - List files in a directory (usage: `%s`)\n", yellow("ls"), yellow("ls <path>"))
-
 	fmt.Printf("    %s         		 - Delete one or more files (usage: `%s`)\n", yellow("rm"), yellow("rm <path> [<path>...]"))
-
 	fmt.Printf("    %s                  - Delete one or more directories (usage: `%s`)\n", yellow("rmd"), yellow("rmd <path> [<path>...]"))
-
 	fmt.Printf("    %s          		 - Quickly navigate to the home directory (usage: `%s`)\n", yellow("/"), yellow("/"))
-
 	fmt.Printf("    %s          	 - Clear the terminal screen (usage: `%s`)\n", yellow("cls"), yellow("cls"))
-
 	fmt.Printf("    %s or %s       - Exit the program (usage: `%s` or `%s`)\n", yellow("exit"), yellow("Crtl+C"), yellow("exit"), yellow("Crtl+C"))
-
 	fmt.Println()
+	return nil
 }
 
-// createDirectory creates a new directory with the specified name.
-func createDirectory(n []string) {
-	for _, v := range n {
-		err := os.Mkdir(v, 0744)
-
+// createDirectory creates one or more directories.
+func createDirectory(paths []string) error {
+	for _, path := range paths {
+		err := os.Mkdir(path, 0744)
 		if err != nil {
-			fmt.Println()
-			fmt.Printf(color.RedString("  >_  Error - mkd "), "`%s`", color.RedString(": Cannot create a file when that file already exists.\n"), yellow(v))
-			fmt.Println()
+			return fmt.Errorf("%s", err)
 		}
 	}
+	return nil
 }
 
-// changeCurrentDirectory changes the current working directory to the specified path.
-func changeCurrentDirectory(path string) {
-	err := os.Chdir(path)
-
-	if err != nil {
-		fmt.Println()
-		fmt.Println(color.RedString("  >_   Error - cd: Unable to access "), yellow(path), color.RedString(" - Directory not found.\n"))
-		fmt.Println()
+// createFile creates one or more files.
+func createFile(paths []string) error {
+	for _, path := range paths {
+		file, err := os.OpenFile(path, os.O_CREATE, 0644)
+		if err != nil {
+			return fmt.Errorf("%s", err)
+		}
+		defer file.Close()
 	}
+	return nil
 }
 
-// listDirectoryContents lists the files and directories in the specified path.
-func listDirectoryContents(path string) {
-	entries, err := os.ReadDir(path)
+// changeCurrentDirectory changes the current working directory.
+func changeCurrentDirectory(args []string) error {
+	if len(args) != 1 {
+		return fmt.Errorf("usage: cd <path>")
+	}
+	return os.Chdir(args[0])
+}
+
+// listDirectoryContents lists the contents of a directory.
+func listDirectoryContents(args []string) error {
+	dir := "."
+	if len(args) > 0 {
+		dir = args[0]
+	}
+
+	entries, err := os.ReadDir(dir)
+	if err != nil {
+		errMessage := fmt.Sprintln(color.RedString("- ls: Unable to access "), yellow(dir), color.RedString(" - Directory not found"))
+
+		return fmt.Errorf("%s", errMessage)
+	}
 
 	fmt.Println()
-	if err != nil {
-		fmt.Println(color.RedString("  >_   Error - ls: Unable to access "), yellow(path), color.RedString(" - Directory not found.\n"))
-	} else if len(entries) == 0 {
-		color.Red("  >_  Empty directory")
+	if len(entries) == 0 {
+		fmt.Println(red("  >_  Empty directory"))
 	} else {
 		for _, entry := range entries {
 			info, err := entry.Info()
-
 			if err != nil {
-				fmt.Println(err)
+				return fmt.Errorf("%s", err)
 			}
 
 			if entry.IsDir() {
 				fmt.Println("  >   ", info.ModTime().Format("02/01/2006"), green("               <dir>                  "), yellow(entry.Name()))
 			} else {
-				fmt.Println("  >   ", info.ModTime().Format("02/01/2006"), blue("               <file>                  "), yellow(entry.Name()))
+				fmt.Println("  >   ", info.ModTime().Format("02/01/2006"), blue("               <file>                 "), yellow(entry.Name()))
 			}
 		}
 	}
 	fmt.Println()
+	return nil
 }
 
-// remove a file with a specified name.
-func removeFile(n []string) {
-	for _, v := range n {
-		err := os.Remove(v)
-
+// removeFile deletes one or more files.
+func removeFile(paths []string) error {
+	for _, path := range paths {
+		err := os.Remove(path)
 		if err != nil {
-			fmt.Println()
-			color.Red("  >_  Error: ", err)
-			fmt.Println()
+			return fmt.Errorf("%s", err)
 		}
 	}
+	return nil
 }
 
-// remove a directory with a specified name.
-func removeDirectory(n []string) {
-	for _, v := range n {
-		err := os.RemoveAll(v)
-
+// removeDirectory deletes one or more directories.
+func removeDirectory(paths []string) error {
+	for _, path := range paths {
+		err := os.RemoveAll(path)
 		if err != nil {
-			fmt.Println()
-			color.Red("  >_  Error: ", err)
-			fmt.Println()
+			return fmt.Errorf("%s", err)
 		}
 	}
+	return nil
 }
 
-// changeToHomeDirectory changes the current directory to the user's home directory.
-func changeToHomeDirectory() {
+// changeToHomeDirectory navigates to the user's home directory.
+func changeToHomeDirectory(_ []string) error {
 	homeDir, err := os.UserHomeDir()
-
 	if err != nil {
-		color.Red("  >_  Error: ", err)
-		return
+		return fmt.Errorf("%v", err)
 	}
-
-	err = os.Chdir(homeDir)
-
-	if err != nil {
-		color.Red("  >_  Error: ", err)
-	}
+	return os.Chdir(homeDir)
 }
 
-func clearScreen() {
+// clearScreen clears the terminal screen.
+func clearScreen(_ []string) error {
 	cmd := exec.Command("cmd", "/c", "cls")
 	cmd.Stdout = os.Stdout
-	cmd.Run()
+	return cmd.Run()
+}
+
+// exitCLI exits the program.
+func exitCLI(_ []string) error {
+	os.Exit(0)
+	return nil
 }
